024846f3ad407731329577d3bdebb0f6dbefcee9
GardunoAlex <monopolyalex80@gmail.com>
2025-01-04 19:47:50 -0600

    functioning as intended

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
-import axios from "axios";
 import pg from "pg";
-// import https from "https";
+import env from "dotenv"
+
 const app = express();
 const port = 3000;
+env.config(); 
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "posts",
-    password: "M1n1p1l1!",
-    port: "5432",
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 
 db.connect();
